76c782a1919f6c22fca4215aa620d110960ec2be
George Park <george95park@gmail.com>
2020-09-24 17:06:53 -0700

    first commit

---
+++ client/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "io"
+    "os"
+    "bufio"
+    "golang.org/x/net/context"
+    "google.golang.org/grpc"
+    pb "chat_grpc/chat"
+)
+var username string
+const addr = "localhost:14586"
+
+func listenForMessages(stream pb.ChatService_CreateStreamClient, chat chan*pb.ChatMessage) {
+    for {
+        in, err := stream.Recv()
+        if err == io.EOF {
+            break
+        }
+        if err != nil {
+            log.Fatalf("Failed to receive message: %v", err)
+        }
+        chat <- in
+    }
+}
+
+func handleInput(scanner *bufio.Scanner, msg chan *pb.ChatMessage) {
+    for scanner.Scan() {
+        m := scanner.Text()
+        req := &pb.ChatMessage {
+            From: username,
+            Message: m,
+        }
+        msg <- req
+    }
+}
+
+func connectUser(ctx context.Context, scanner *bufio.Scanner, c pb.ChatServiceClient) (pb.ChatService_CreateStreamClient) {
+    fmt.Println("Enter an username: ")
+    scanner.Scan()
+    userInput := scanner.Text()
+    u := &pb.Connect{
+        Name: userInput,
+    }
+    username = userInput
+    stream, err := c.CreateStream(ctx, u)
+    if err != nil {
+        log.Fatalf("error creating stream: %v", err)
+    }
+    fmt.Printf("Connected\n")
+    return stream
+}
+
+func main() {
+    msg := make(chan *pb.ChatMessage)
+    chat := make(chan *pb.ChatMessage)
+
+    conn, err := grpc.Dial(addr, grpc.WithInsecure())
+    fmt.Println("Connecting to gRPC server...")
+    if err != nil {
+        log.Fatalf("Did not connect: %v", err)
+    }
+    fmt.Println("Connected to gRPC!")
+    defer conn.Close()
+
+    client := pb.NewChatServiceClient(conn)
+    ctx := context.Background()
+    scanner := bufio.NewScanner(os.Stdin)
+    stream := connectUser(ctx, scanner, client)
+    go listenForMessages(stream, chat)
+    go handleInput(scanner, msg)
+
+    for {
+        select {
+            case m := <-msg:
+                if _,err := client.BroadcastMessage(ctx, m); err != nil {
+                    log.Fatalf("Failed to send message: %v", err)
+                }
+            case c := <-chat:
+                fmt.Printf("> %v: %v\n", c.From, c.Message)
+        }
+    }
+}
---
+++ server/main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net"
+    "google.golang.org/grpc"
+    "golang.org/x/net/context"
+    pb "chat_grpc/chat"
+)
+
+const port = ":14586"
+
+type user struct {
+    stream pb.ChatService_CreateStreamServer
+    name string
+    error chan error
+}
+
+type server struct {
+    observers []*user
+}
+
+func (s* server) CreateStream(conn *pb.Connect, stream pb.ChatService_CreateStreamServer) error {
+    u := &user {
+        stream: stream,
+        name: conn.Name,
+        error: make(chan error),
+    }
+    s.observers = append(s.observers, u)
+    log.Printf("User: %v has connected", conn.Name)
+    return <-u.error
+}
+
+func (s* server) BroadcastMessage(ctx context.Context, msg *pb.ChatMessage) (*pb.Empty, error) {
+    for _,obs := range s.observers {
+        err := obs.stream.Send(msg)
+        if err != nil {
+            return &pb.Empty{}, err
+        }
+        log.Printf("Broadcasting from: %v -- Message: %v", msg.From, msg.Message)
+    }
+    return &pb.Empty{}, nil
+}
+
+// func (s *server) Chat(stream pb.ChatService_ChatServer) error {
+//     waitc := make(chan *pb.ChatMessage)
+//     go func() {
+//         for {
+//             req, err := stream.Recv()
+//             if err == io.EOF {
+//                 close(waitc)
+//                 break
+//             }
+//             if err != nil {
+//                 log.Fatalf("error receiving: ", err)
+//                 break
+//             }
+//             log.Printf("received request: %v", req)
+//             if s.observers == nil {
+//                 s.observers = make(map[pb.ChatService_ChatServer]bool)
+//             }
+//             s.observers[stream] = true
+//             waitc <- req
+//         }
+//     }()
+//     for m := range waitc {
+//         for obs,_ := range s.observers {
+//             err := obs.Send(&pb.ChatMessageFromServer{
+//                 Message: &pb.ChatMessage{
+//                     From: m.From,
+//                     Message: m.Message,
+//                 },
+//             })
+//             if err != nil {
+//                 return err
+//             }
+//         }
+//     }
+//     return nil
+// }
+
+func main() {
+    fmt.Printf("Starting server on port%v...\n", port)
+    lis, err := net.Listen("tcp", port)
+    if err != nil {
+        log.Fatalf("Failed to listen: %v", err)
+    }
+    fmt.Println("Server started!")
+    s := grpc.NewServer()
+    pb.RegisterChatServiceServer(s, &server{})
+    s.Serve(lis)
+}
